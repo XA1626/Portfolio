e0bf3bc681e87a111061f7e5f7c2f75a683fbdd0
XA1626 <132309726+XA1626@users.noreply.github.com>
2025-07-14 19:40:20 +1200

    V 0.2.3

--- data/projects.php
+++ data/projects.php
@@ -1,3 +1,11 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $project['title'] . " | Dexter's Portfolio"; ?></title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="<?php echo dirname($_SERVER['PHP_SELF']) === '/' ? 'style.css' : '../style.css'; ?>">
+</head>
+
 <?php // data/projects.php
 return [
     'software' => [
